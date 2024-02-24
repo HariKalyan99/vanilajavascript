0b7ac74e2532eab8493906814680844534cfc2a2
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-02-24 21:52:52 +0530

    js

---
+++ index.js
@@ -0,0 +1,5 @@
+// console.log(document.getElementById("helloButton"));
+// console.log(document.getElementsByClassName("howButton"));
+// console.log(document.querySelectorAll(".howButton"));
+
+let count = 0;
